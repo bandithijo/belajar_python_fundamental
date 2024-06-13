605d89a50965376906de82d2027f61086f29825e
Rizqi Nur Assyaufi <bandithijo@gmail.com>
2024-06-14 06:12:54 +0800

    00 hello world

---
+++ 00_hello_world.py
@@ -0,0 +1,10 @@
+# hello_world
+
+print("Hello, World!")
+print("Hello, Python World!")
+print("Saya sedang belajar Python.")
+print(100_000)
+print(2024)
+print(50.24)
+print(True)
+print("*" * 10)
