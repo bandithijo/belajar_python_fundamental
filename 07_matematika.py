8a2ec955aa257c5f3bd4cc1d5e91063a4e51d5c8
Rizqi Nur Assyaufi <bandithijo@gmail.com>
2024-06-14 21:36:04 +0800

    07 matematika

---
+++ 07_matematika.py
@@ -0,0 +1,18 @@
+# matematika
+
+x = 10
+y = 5
+
+print(x + y)   # penjumlahan
+print(x - y)   # pengurangan
+print(x * y)   # perkalian
+print(x ** y)  # perpangkatan
+print(x / y)   # pembagian dengan float return value
+print(x // y)  # pembagian dengan integer return value
+print(x % y)   # modulus
+
+x += 5
+print(x)       # 15
+
+x -= 5
+print(x)       # 10
