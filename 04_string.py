5a0ea7b974d984e82fbe3a050a417ac95474d122
Rizqi Nur Assyaufi <bandithijo@gmail.com>
2024-06-14 17:56:36 +0800

    04 string

---
+++ 04_string.py
@@ -0,0 +1,23 @@
+# string
+
+first_name = 'Rizqi'
+last_name = "Assyaufi"
+
+print(first_name[0])
+print(first_name[0:])
+
+print(first_name + " " + last_name)
+print(first_name, last_name)
+print("%s %s" % (first_name, last_name))
+print(f"{first_name} {last_name}")
+
+print("-" * 20)
+
+full_name = first_name + " " + last_name
+
+print(full_name)
+print(full_name[6:])
+print(full_name[6:-3])
+print(full_name[2:11])
+print(full_name[:11])
+print(full_name[:])
