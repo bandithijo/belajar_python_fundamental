09d195d38ebeed2bcc9b8a534098c528ea8c0c35
Rizqi Nur Assyaufi <bandithijo@gmail.com>
2024-06-15 00:35:05 +0800

    13 perulangan while

---
+++ 13_perulangan_while.py
@@ -0,0 +1,19 @@
+# perulangan while
+
+index = 1
+
+while index <= 5:
+    print(f"(index ke {index}) Go")
+    index += 1
+
+print(f"(index ke {index}) Stop")
+
+print("-" * 80)
+
+index = 1
+
+while index <= 5:
+    print("*" * index)
+    index += 1
+
+print("*" * index)
