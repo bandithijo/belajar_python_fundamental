e9fa5d64156ca88509b4edc87ef59af68cdef302
Rizqi Nur Assyaufi <bandithijo@gmail.com>
2024-06-14 08:47:23 +0800

    01 variabel dan tipe data

---
+++ 01_variable_dan_tipe_data.py
@@ -0,0 +1,20 @@
+# variabel dan tipe data
+
+"""
+tipe data di Python bersifat dynamic type.
+"""
+
+price = 100_000
+print(price)
+
+discount = 0.5
+print(discount)
+
+is_promo = True
+print(is_promo)
+
+product_name = "Gundam"
+print(product_name)
+
+total_price = price * discount
+print(total_price)
