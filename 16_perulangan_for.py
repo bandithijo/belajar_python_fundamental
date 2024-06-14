aea4f8950410eb5f02f0090a36e1268470f7d7dc
Rizqi Nur Assyaufi <bandithijo@gmail.com>
2024-06-15 01:11:45 +0800

    16 perulangan for

---
+++ 16_perulangan_for.py
@@ -0,0 +1,26 @@
+# perulangan for
+
+# perulangan for untuk string
+name = "Rizqi Assyaufi"
+
+for n in name:
+    print(n)
+
+
+# perulangan for untuk list
+numbers = [1, 2, 3, 4, 5, 6]
+
+for number in numbers:
+    print(number)
+    print(number * 10)
+
+
+# perulangan for dengan function range()
+for number in range(10):
+    print(number)
+
+for number in range(1, 11):
+    print(number)
+
+for number in range(1, 11, 2):
+    print(number)
