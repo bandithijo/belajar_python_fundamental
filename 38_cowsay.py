0a6b8ead69c4d0d16753eed2e47822d701898d19
Rizqi Nur Assyaufi <bandithijo@gmail.com>
2024-06-16 23:13:25 +0800

    38 virtual env

---
+++ 38_cowsay.py
@@ -0,0 +1,24 @@
+# virtual env
+
+"""
+1. buat virtualenv dengan cara,
+   ```
+   $ virtualenv venv
+   ```
+2. aktifkan virtualenv venv dengan cara,
+   ```
+   $ source venv/bin/activate
+   ```
+
+3. untuk keluar dari virtualenv venv dengan cara,
+   ```
+   $ deactivate
+   ```
+"""
+import cowsay
+
+cowsay.cow("Hello, Python!")
+
+cowsay.dragon("Hello, Python!")
+
+cowsay.ghostbusters("Hello, Python!")
